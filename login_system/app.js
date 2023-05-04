5d71b41b59fb6757784d30db2aefdb4deb1ae15d
Anirudh Kala <122237860+anirudhkala110@users.noreply.github.com>
2023-05-05 04:23:52 +0530

    Add files via upload

--- login_system/app.js
+++ login_system/app.js
@@ -21,10 +21,13 @@ db.connect((err) => {
     if (err) throw err
     else {
         console.log("MySQL Connection successful..")
-        var sql = "CREATE TABLE project_final(id INT(5),Name VARCHAR(255),Phone_Number INT(11),Email VARCHAR(100),Password VARCHAR(100))"
-        db.query(sql, function (err, result) {
+        var sqllogin = "CREATE TABLE project_final(id INT(5),Name VARCHAR(255),Phone_Number INT(11),Email VARCHAR(100),Password VARCHAR(100))"
+        db.query(sqllogin, function (err, result) {
             if (err) {
-                console.log("Table Already Exists...");
+                console.log("Table Login Already Exists...");
+            }
+            else {
+                console.log("Table project_final Created :)");
                 //var againsql = "INSERT INTO project_final(id,Name,Phone_Number,Email,Password) VALUES ('1','Anirudh Kala','123456789','anirudhkala@gmail.com','Akala@12345')"
                 //db.query(againsql, function (err, result) {
                 //    if (err)
@@ -33,8 +36,32 @@ db.connect((err) => {
                 //        console.log("Data inserted : ", result);
                 //})
             }
+        })
+        var sqldepartmentnotices = "CREATE TABLE department_notice(id INT(5),Name VARCHAR(255),Phone_Number INT(11),Email VARCHAR(100),Password VARCHAR(100))"
+        db.query(sqldepartmentnotices, function (err, result) {
+            if (err) {
+                console.log("Table department_notice Already Exists...");
+            }
             else {
-                console.log("Table Created");
+                console.log("Table department_notice Created:)");
+            }
+        })
+        var sqlplacementnotices = "CREATE TABLE placement_notice(id INT(5),topic VARCHAR(255),detail LONGTEXT,image BLOB,date DATE)"
+        db.query(sqlplacementnotices, function (err, result) {
+            if (err) {
+                console.log("Table placement_notice Already Exists...");
+            }
+            else {
+                console.log("Table placement_notice Created:)");
+            }
+        })
+        var sqlplacementresultnotice = "CREATE TABLE placement_result_notice(id INT(5),topic VARCHAR(255),detail LONGTEXT,image BLOB,date DATE)"
+        db.query(sqlplacementresultnotice, function (err, result) {
+            if (err) {
+                console.log("Table placement_result_notice Already Exists...");
+            }
+            else {
+                console.log("Table placement_result_notice Created:)");
             }
         })
     }
