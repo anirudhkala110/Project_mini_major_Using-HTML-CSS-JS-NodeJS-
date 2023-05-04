0d72403b42e1c46edd74b510da3d224537cc76e1
Anirudh Kala <122237860+anirudhkala110@users.noreply.github.com>
2023-05-05 04:20:47 +0530

    Add files via upload

--- login_system/public/js/script.js
+++ login_system/public/js/script.js
@@ -1,13 +1,12 @@
 const btun = document.getElementById("studentcornerbtn")
 const list = document.getElementById("dropdownitems")
-console.log("working")
 list.style.display = "none";
 
-btun.addEventListener('click',(event)=>{
-    if(list.style.display === "none"){
+btun.addEventListener('click', (event) => {
+    if (list.style.display === "none") {
         list.style.display = "block";
     }
-    else{
+    else {
         list.style.display = "none";
     }
 })
